0de58348df7bb56b01a7d6b43aae5b22109d4d41
user7 <im.krista69@gmail.com>
2024-12-21 15:00:19 +0300

    added timer for task

--- src/components/label/label.jsx
+++ src/components/label/label.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
 
-const Label = ({ task, date }) => {
-  return (
-    <label>
-      <span className="description">{task}</span>
-        <span className="description">{task}</span>
-      <span className="created">{`created ${formatDistanceToNow(date, Date.now())} ago`}</span>
-    </label>
-  );
-};
-export default Label;
+export default class Label extends React.Component {
+  state = {
+    time: 0,
+  };
+
+  updateTime() {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  }
+
+  startTimer = () => {
+    this.pauseTimer();
+    this.timerID = setInterval(() => this.updateTime(), 1000);
+  };
+
+  pauseTimer = () => {
+    clearInterval(this.timerID);
+  };
+
+  render() {
+    const { task, date } = this.props;
+    return (
+      <label>
+        <span className="title">{task}</span>
+        <span className="description">
+          <button className="icon icon-play" onClick={this.startTimer}></button>
+          <button className="icon icon-pause" onClick={this.pauseTimer}></button>
+          {` ${Math.floor(this.state.time / 60)}:${this.state.time % 60} `}
+        </span>
+        <span className="description">{`created ${formatDistanceToNow(date, Date.now())} ago`}</span>
+      </label>
+    );
+  }
+}
